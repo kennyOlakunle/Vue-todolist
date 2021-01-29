07d23f14af46542b0c07da8f31a841bb30c5ec59
kennyOlakunle <olakunlemuyiwa@gmail.com>
2021-01-30 00:01:57 +0100

    adding Header and Items

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,23 @@
 <template>
     <div class="todo-item" v-bind:class="{'is-completed':todo.completed}">
-        <p>{{todo.title}}</p>
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+            <button @click="$emit('del-todo', todo.id)" class="del">X</button>
+            </p>
+
     </div>
 </template>
 
 <script>
 export default {
     name: "TodoItem",
-    props: ["todo"]
+    props: ["todo"], 
+    methods: {
+        markComplete(){
+                this.todo.completed = ! this.todo.completed
+        }
+    }
 }
 </script>
 
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+     <h1>
+         TodoList
+     </h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style scoped>
+    
+</style>
