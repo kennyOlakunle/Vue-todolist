7d542e825616cfb56cd95c9047a8ec83136b36eb
kennyOlakunle <olakunlemuyiwa@gmail.com>
2021-01-30 00:47:00 +0100

    Update/Post feature

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,48 @@
 <template>
   <div id="app">
-    
+    <Header />
+    <AddTodo v-on:add-todo="addTodo"/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
     
 
 <script>
-
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import Header from './components/layout/Header'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    Todos
+    Todos,
+    Header,
+    AddTodo
     
   }, 
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-
-        },
-      ]
+      todos: []
 }
 },
 
 methods: {
   deleteTodo(id){
     this.todos = this.todos.filter(todo => todo.id !== id)
+  },
+
+  addTodo(newTodo) {
+    this.todos = [...this.todos, newTodo]
   }
+},
+
+created(){
+axios.get(`https://jsonplaceholder.typicode.com/todos`)
+.then(res => this.todos = res.data)
+.catch(err => console.log(err))
 }
+
 }
 </script>
 
@@ -58,5 +56,18 @@ methods: {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
   }
 </style>
